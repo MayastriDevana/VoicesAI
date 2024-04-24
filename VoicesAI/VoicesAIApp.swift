5abec04f0ecd472ae9e6ef08680b6d3cdb11d835
Maya <Mayastridevana0205@gmail.com>
2024-04-24 13:20:43 +0700

    Initial Commit

---
+++ VoicesAI/VoicesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicesAIApp.swift
+//  VoicesAI
+//
+//  Created by User on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VoicesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
